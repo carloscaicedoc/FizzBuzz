b556082be114e9d5fc48c2bf8bd27aeea266ce0a
Carlos Caicedo <calicheccmusica@gmail.com>
2021-12-20 12:47:17 -0800

    final commit

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,7 +2,7 @@
 For each number that is a multiple of 3, print "Fizz"
 For each number that is a multiple of 5, print "Buzz"
 For numbers which are a multiple of both 3 and 5, print "FizzBuzz"
-All other numbers should just print normally */
+All other numbers should just print normally  */
 
 for (var i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0){
